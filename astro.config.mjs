9ed2bac4c848ca516a18949c2e3edf50e2ad9a9c
William Hallin <william.hallin@newgent.digital>
2023-10-15 03:27:25 +0200

    fix: prefetch issue, selection color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,30 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://whsmedia.se",
-  integrations: [
-    sitemap({
-      i18n: {
-        defaultLocale: "sv",
-        locales: {
-          sv: "sv-SE",
-        },
-      },
-    }),
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "sv",
+      locales: {
+        sv: "sv-SE"
+      }
+    }
+  }), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), prefetch({
+    throttle: 3,
+  })],
   image: {
-    remotePatterns: [
-      {
-        protocol: "https",
-      },
-    ],
-  },
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  }
 });
